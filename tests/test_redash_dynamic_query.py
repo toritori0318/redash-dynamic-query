ec373a4cfb05953fac3f0a44d4054f5b731996ee
ariarijp <takuya.arita@gmail.com>
2017-12-30 11:06:02 +0900

    Fix _bind_params for unescaped values

--- tests/test_redash_dynamic_query.py
+++ tests/test_redash_dynamic_query.py
@@ -24,6 +24,11 @@ class TestRedashDynamicQuery(unittest.TestCase):
             redash._bind_params('SELECT id REGEXP \'^[0-9]{1,4}\' FROM users WHERE id={{user_id}};', bind),
             'SELECT id REGEXP \'^[0-9]{1,4}\' FROM users WHERE id=123;')
 
+        bind = {'user_name': '\'johndoe\'', 'age_condition': 'age>35'}
+        self.assertEqual(
+            redash._bind_params('SELECT * FROM users WHERE name={{{user_name}}} AND {{{age_condition}}};', bind),
+            'SELECT * FROM users WHERE name=\'johndoe\' AND age>35;')
+
 
 if __name__ == '__main__':
     unittest.main()
